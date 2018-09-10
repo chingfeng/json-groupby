101c34479d4092dfd9da0cc2356c6720f5ec9ad1
Wen-Hao Chang <wenhao.chang@gigabyte.com>
2018-09-10 22:55:03 +0800

    Improve: return prop "__KEY_NON_EXIST" if key not founded

--- property-value.js
+++ property-value.js
@@ -1,23 +1,25 @@
 function valueAt(obj, path) {
   var parts = path.split('.')
   var prop = parts.shift()
-  if (isPrimitive(obj[prop])) {
+  if (!obj[prop]) {
+    return ['__KEY_NON_EXIST']
+  } else if (isPrimitive(obj[prop])) {
     return [obj[prop]]
   } else if (isObject(obj[prop])) {
     return valueAt(obj[prop], parts.join('.'))
   } else if (isPrimitiveArray(obj[prop])) {
     return obj[prop].reduce(function(values, val) {
-			if (values.indexOf(val) < 0) values.push(val)
-      return values
+      if (values.indexOf(val) < 0) values.push(val)
+        return values
     },[])
   } else if (isObjectArray(obj[prop])) {
     return obj[prop].reduce(function(values, item) {
       var vals = valueAt(item, parts.join('.'))
       vals.reduce(function(sum, key) {
-				if (sum.indexOf(key) < 0) sum.push(key)
-        return sum
+        if (sum.indexOf(key) < 0) sum.push(key)
+          return sum
       }, values)
-			return values
+      return values
     }, [])
   } else {
     throw new TypeError('property path: "'+ path + '" not valid in your data at object: '+ JSON.stringify(obj))
